c27c919b5b9bf481514b824feeb0963c4b6f3e60
sarbjit <sarbjitdeol72@gmail.com>
2023-11-29 12:03:40 +0530

    all

--- accounts/views.py
+++ accounts/views.py
@@ -247,6 +247,170 @@ def upload_excel(request):
                         print(e)
                     logout()
             execute_sportybet()
+        if selected_bot == "B":
+            xbet_accounts = []
+            accounts = UserAccount.objects.all().values_list('email', 'username', 'password', 'website')
+            accounts_list = list(accounts)
+            accounts = [{'email': tup[0], 'username': tup[1], 'password': tup[2], 'betting_site': tup[3]} for tup in
+                        accounts_list]
+            print(accounts)
+            for i in accounts:
+                if i["betting_site"] == "Xbet":
+                    xbet_accounts.append(i)
+            options = Options()
+            options.add_argument('--disable-gpu')
+            # options.add_argument('--headless')
+            browser = webdriver.Chrome()
+
+            # 1xbet login xpaths
+            xPathLogin = '//*[@id="j_page_header"]/div[1]/div/div[1]/div[1]/div[2]/div[3]/div[1]/button'
+            xPathEmail = '//*[@id="j_page_header"]/div[1]/div/div[1]/div[1]/div[2]/div[2]/div[1]/input'
+            xPathPassword = '//*[@id="j_page_header"]/div[1]/div/div[1]/div[1]/div[2]/div[3]/div[1]/input'
+            xPathSubmitLogin = '//*[@id="j_page_header"]/div[1]/div/div[1]/div[1]/div[2]/div[3]/div[1]/button'
+            # 1xbet login successful confirmation xpath
+            xPathMyAccount = '//*[@id="j_userInfo"]/span'
+
+            def placebet(url, bet_columns, username, password):
+                browser.get(url)
+                time.sleep(1)
+                try:
+                    browser.find_element(By.XPATH, xPathLogin).click()
+                    time.sleep(3)
+
+                    usernameEl = browser.find_element(By.XPATH, xPathEmail)
+                    passwordEl = browser.find_element(By.XPATH, xPathPassword)
+                    usernameEl.clear()
+                    usernameEl.send_keys(username)
+                    time.sleep(1)
+                    passwordEl.send_keys(password)
+                    time.sleep(1)
+                    browser.find_element(By.XPATH, xPathSubmitLogin).click()
+                    WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.XPATH, xPathMyAccount)))
+                    time.sleep(2)
+
+
+                except :
+                    pass
+
+                correct_score = browser.find_element(By.XPATH, "//*[text()='Correct Score']")
+                correct_score.click()
+                table = browser.find_element(By.XPATH,
+                                                "//span[text()='Correct Score']/ancestor::div[1]/ancestor::div[1]/ancestor::div[1]/following-sibling::div")
+
+                
+                bets = table.find_elements(By.XPATH, ".//span[@class='m-table-cell-item']")
+                for bet in bets:
+                    if bet.text == bet_columns:
+                        print(f' Bet Column: {bet_columns}, Bet: {bet.text}')
+                        bet.click()
+                print(bet_columns)
+
+            def logout():
+                logout = browser.find_element(By.XPATH, '//*[@id="j_userInfo"]/span')
+                logout.click()
+                time.sleep(1)
+                logout_btn = browser.find_element(By.XPATH, '//*[@id="j_userInfo"]/ul/li[8]')
+                logout_btn.click()
+                print('log out')
+                time.sleep(3)
+            def execute_bet():
+                print('execute_bet')
+                time.sleep(4)
+                browser.execute_script("window.scrollTo(0, 0);")
+                count = browser.find_element(By.XPATH, './/span[@class="m-input-com"]/input')
+                count.click()
+                count.send_keys(Keys.CONTROL + "a")
+                count.send_keys(Keys.BACKSPACE)
+                count.send_keys('0.1')
+                time.sleep(2)
+                place = browser.find_element(By.XPATH, '//*[@id="j_betslip"]//button')
+                place.click()
+                print('place bet click')
+                time.sleep(7)
+                place.click()
+                confirm = browser.find_element(By.XPATH, "//*[text()='Confirm']")
+                confirm.click()
+                time.sleep(3)
+                ok = browser.find_element(By.XPATH, "//*[text()='OK']")
+                ok.click()
+                time.sleep(1)
+                
+            def cashout_func():
+                cashout_count = browser.find_element(By.XPATH,
+                                                    '//*[@id="j_betslip"]/div[1]/div[1]/div[3]/div/span[2]').text
+                print('cashout_count:', cashout_count)
+                if int(cashout_count) >= 1:
+                    for i in range(0, int(cashout_count)):
+                        time.sleep(3)
+                        cashout_button = browser.find_element(By.XPATH, '//*[@id="j_betslip"]/div[1]/div[1]/div[3]')
+                        cashout_button.click()
+                        time.sleep(3)
+                        cashout = browser.find_element(By.XPATH,
+                                                    '//*[@id="j_betslip"]/div[3]/ul/li/div[2]/div[4]/div[2]/div[5]/button')
+                        cashout.click()
+                        time.sleep(1)
+                        cashout_confirm = browser.find_element(By.XPATH,
+                                                            '//*[@id="j_betslip"]/div[3]/ul/li/div[2]/div[4]/div[2]/div[1]/div[2]/button[2]/span/span')
+                        cashout_confirm.click()
+                        time.sleep(3)
+                        print(f'Cashout done {i}')
+
+            def execute_Xbet():
+                csv_file_path = uploaded_file
+                results = []
+                df = pd.read_csv(csv_file_path, header=None)
+                total_rows, total_columns = df.shape
+                print(uploaded_file)
+                print(total_rows)
+                print(total_columns)
+                all_results = []
+
+                print('xbet_accounts',len(xbet_accounts))
+               # Assuming you have 2 accounts
+                num_accounts = len(xbet_accounts)
+                columns_per_account = df.shape[1] // num_accounts
+
+                # Iterate through each account
+                for account_index, account in enumerate(xbet_accounts):
+                    username = account["username"]
+                    password = account["password"]
+
+                    try:
+                        # Remove empty strings and single quotes
+                        urls_list = [item.strip("'") for item in urls if item]
+                        
+                        # Determine the range of columns for the current account
+                        start_col = account_index * columns_per_account
+                        end_col = (account_index + 1) * columns_per_account
+
+                        # Iterate through columns for the current account
+                        for col_index in range(start_col, end_col):
+                            # Get the bets for the current column
+                            bets = df.iloc[:, col_index]
+
+                            # Iterate through rows and place bets
+                            for j, bet in enumerate(bets):
+                                # Use the value from the corresponding column and row
+                                url = urls[j]  # Use the URL corresponding to the current row
+                                print(f'Account: {username}, URL: {url}, Column: {col_index + 1}, Row: {j + 1}, Bet: {bet}')
+                                try:
+                                    placebet(url, bet, username, password)
+                                except Exception as e:
+                                    print(f'Error placing bet: {e}')
+
+                                time.sleep(2)
+
+        
+
+                                # Execute the betslip function after completing all URLs for a column
+                            execute_bet()
+                    
+                            
+                            # cashout_func()
+                    except Exception as e:
+                        print(e)
+                    logout()
+            execute_Xbet()
         if selected_bot == "C":
             print('Betway')
             betway_accounts = []
